6789180f09ca417b6c89abd8785edc349898d176
Alan Mong <alanmongx@gmail.com>
2024-08-13 02:28:56 -0700

    create get request

--- app.js
+++ app.js
@@ -12,12 +12,9 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    "GET, POST, PUT, DELETE, PATCH,"
   );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Origin, X-Requested-With"
-  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization,");
   next();
 });
 
